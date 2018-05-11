aaf512f0e6e6693ba98677636d82180b522c647a
hokop <hokop@DESKTOP-MJIBMTO>
2018-05-11 14:06:40 +0900

    Search 예제 추가

---
+++ CommentSearchWeb/src/main/java/com/smh/cs/SearchSvc.java
@@ -0,0 +1,14 @@
+package com.smh.cs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SearchSvc {
+
+	private static final Logger logger = LoggerFactory.getLogger(SearchSvc.class);
+	
+	
+	
+}
---
+++ CommentSearchWeb/src/test/java/com/smh/cs/ApiTest.java
@@ -0,0 +1,10 @@
+package com.smh.cs;
+
+public class ApiTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
