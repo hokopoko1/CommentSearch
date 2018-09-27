e6c06da5621967150de3e63fcd17c541a86f2cf0
hokop <hokop@124.111.196.176>
2018-09-27 21:26:45 +0900

    added now file

---
+++ CommentSearchWeb/src/main/java/com/smh/cs/model/ChatInfo.java
@@ -0,0 +1,33 @@
+package com.smh.cs.model;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ChatInfo {
+
+	String publishedAt;
+	String displayMessage;
+	String displayName;
+	
+	public String getPublishedAt() {
+		return publishedAt;
+	}
+	public void setPublishedAt(String publishedAt) {
+		this.publishedAt = publishedAt;
+	}
+	public String getDisplayMessage() {
+		return displayMessage;
+	}
+	public void setDisplayMessage(String displayMessage) {
+		this.displayMessage = displayMessage;
+	}
+	public String getDisplayName() {
+		return displayName;
+	}
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+	
+}
