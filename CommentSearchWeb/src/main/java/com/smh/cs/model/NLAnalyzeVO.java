9a27159f6f409857312f98eb495e4405cc96df8f
hokop <hokop@124.111.196.176>
2018-10-03 15:27:47 +0900

    latest

---
+++ CommentSearchWeb/src/main/java/com/smh/cs/model/NLAnalyzeVO.java
@@ -0,0 +1,21 @@
+package com.smh.cs.model;
+
+public class NLAnalyzeVO {
+
+	String nouns;
+	String adjs;
+	
+	public String getNouns() {
+		return nouns;
+	}
+	public void setNouns(String nouns) {
+		this.nouns = nouns;
+	}
+	public String getAdjs() {
+		return adjs;
+	}
+	public void setAdjs(String adjs) {
+		this.adjs = adjs;
+	}
+	
+}
---
+++ CommentSearchWeb/src/main/java/com/smh/util/NLAnalyze.java
@@ -0,0 +1,90 @@
+package com.smh.util;
+
+import com.google.cloud.language.v1beta2.*;
+import com.smh.cs.model.NLAnalyzeVO;
+
+import java.util.List;
+
+public class NLAnalyze {
+
+	private static NLAnalyze instance = new NLAnalyze();
+	private static LanguageServiceClient languageServiceClient;
+
+	public static NLAnalyze getInstance() {
+		try {
+			languageServiceClient = LanguageServiceClient.create();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return instance;
+	}
+
+	public NLAnalyzeVO analyze(String text) {
+
+		NLAnalyzeVO nlAnalyzeVo = new NLAnalyzeVO();
+
+		List<Token> tokenList = analyzeSyntax(text);
+
+		for (Token token : tokenList) {
+			String tag = token.getPartOfSpeech().getTag().toString();
+			String content = token.getText().getContent();
+
+			if (tag.equals("NOUN"))
+				nlAnalyzeVo.setNouns(content);
+			else if (tag.equals("ADJ"))
+				nlAnalyzeVo.setAdjs(content);
+		}
+
+		return nlAnalyzeVo;
+	}
+
+	public List<Entity> analyzeEntities(String text) {
+		try {
+			Document document = Document.newBuilder().setContent(text).setType(Document.Type.PLAIN_TEXT).build();
+
+			AnalyzeEntitiesRequest request = AnalyzeEntitiesRequest.newBuilder().setDocument(document)
+					.setEncodingType(EncodingType.UTF16).build();
+
+			AnalyzeEntitiesResponse response = languageServiceClient.analyzeEntities(request);
+
+			return response.getEntitiesList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public Sentiment analyzeSentiment(String text) {
+		try {
+			Document document = Document.newBuilder().setContent(text).setType(Document.Type.PLAIN_TEXT).build();
+
+			AnalyzeSentimentResponse response = languageServiceClient.analyzeSentiment(document);
+			return response.getDocumentSentiment();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public List<Token> analyzeSyntax(String text) {
+		try {
+			Document document = Document.newBuilder().setContent(text).setType(Document.Type.PLAIN_TEXT).build();
+
+			AnalyzeSyntaxRequest request = AnalyzeSyntaxRequest.newBuilder().setDocument(document)
+					.setEncodingType(EncodingType.UTF16).build();
+
+			AnalyzeSyntaxResponse response = languageServiceClient.analyzeSyntax(request);
+
+			return response.getTokensList();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+}
